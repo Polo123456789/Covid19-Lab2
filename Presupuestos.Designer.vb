fa63c05698c282ee12d7c4bf6683065fa9bbeb40
MassielDieguez <massiel.dieguez@gmail.com>
2020-05-27 13:30:03 -0600

    Se realizaron cambios en el formulario de presupuesto, editando visualizacion para el usuario y asignacion de nombre a labels, textbox. Se añadió el botón finalizar. Que permite regresar al menu despues de terminar la transaccion.

--- Presupuestos.Designer.vb
+++ Presupuestos.Designer.vb
@@ -22,212 +22,240 @@ Partial Class Presupuestos
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.ListBox3 = New System.Windows.Forms.ListBox()
-        Me.ListBox4 = New System.Windows.Forms.ListBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lblProductosLista = New System.Windows.Forms.Label()
+        Me.lblPresupuesto = New System.Windows.Forms.Label()
+        Me.lblNoPedido = New System.Windows.Forms.Label()
+        Me.txtPresupuesto = New System.Windows.Forms.TextBox()
+        Me.txtNoPedido = New System.Windows.Forms.TextBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.lblPrecioU = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblLugar = New System.Windows.Forms.Label()
+        Me.lstNombre = New System.Windows.Forms.ListBox()
+        Me.lstCantidad = New System.Windows.Forms.ListBox()
+        Me.lstPrecio = New System.Windows.Forms.ListBox()
+        Me.lstSubtotal = New System.Windows.Forms.ListBox()
+        Me.cmbLugar = New System.Windows.Forms.ComboBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnFinalizar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblProductosLista
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(329, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(219, 31)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Productos a tu lista"
+        Me.lblProductosLista.AutoSize = True
+        Me.lblProductosLista.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblProductosLista.Font = New System.Drawing.Font("Century Gothic", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProductosLista.ForeColor = System.Drawing.Color.MediumBlue
+        Me.lblProductosLista.Location = New System.Drawing.Point(253, 9)
+        Me.lblProductosLista.Name = "lblProductosLista"
+        Me.lblProductosLista.Size = New System.Drawing.Size(334, 43)
+        Me.lblProductosLista.TabIndex = 0
+        Me.lblProductosLista.Text = "Productos a tu lista"
         '
-        'Label2
+        'lblPresupuesto
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(230, 79)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(66, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Presupuesto"
+        Me.lblPresupuesto.AutoSize = True
+        Me.lblPresupuesto.Font = New System.Drawing.Font("Gill Sans MT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPresupuesto.Location = New System.Drawing.Point(79, 86)
+        Me.lblPresupuesto.Name = "lblPresupuesto"
+        Me.lblPresupuesto.Size = New System.Drawing.Size(154, 38)
+        Me.lblPresupuesto.TabIndex = 1
+        Me.lblPresupuesto.Text = "Presupuesto"
         '
-        'Label3
+        'lblNoPedido
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(491, 79)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(57, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "No.Pedido"
+        Me.lblNoPedido.AutoSize = True
+        Me.lblNoPedido.Font = New System.Drawing.Font("Gill Sans MT", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNoPedido.Location = New System.Drawing.Point(450, 81)
+        Me.lblNoPedido.Name = "lblNoPedido"
+        Me.lblNoPedido.Size = New System.Drawing.Size(135, 38)
+        Me.lblNoPedido.TabIndex = 2
+        Me.lblNoPedido.Text = "No.Pedido"
         '
-        'TextBox1
+        'txtPresupuesto
         '
-        Me.TextBox1.Location = New System.Drawing.Point(304, 76)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 3
+        Me.txtPresupuesto.Location = New System.Drawing.Point(239, 86)
+        Me.txtPresupuesto.Multiline = True
+        Me.txtPresupuesto.Name = "txtPresupuesto"
+        Me.txtPresupuesto.Size = New System.Drawing.Size(159, 33)
+        Me.txtPresupuesto.TabIndex = 3
         '
-        'TextBox2
+        'txtNoPedido
         '
-        Me.TextBox2.Location = New System.Drawing.Point(551, 76)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 4
+        Me.txtNoPedido.Location = New System.Drawing.Point(606, 86)
+        Me.txtNoPedido.Multiline = True
+        Me.txtNoPedido.Name = "txtNoPedido"
+        Me.txtNoPedido.Size = New System.Drawing.Size(78, 33)
+        Me.txtNoPedido.TabIndex = 4
         '
-        'Label4
+        'lblNombre
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(69, 164)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(89, 13)
-        Me.Label4.TabIndex = 5
-        Me.Label4.Text = "Nombre producto"
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Font = New System.Drawing.Font("Gill Sans MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNombre.Location = New System.Drawing.Point(26, 167)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(173, 30)
+        Me.lblNombre.TabIndex = 5
+        Me.lblNombre.Text = "Nombre Producto"
         '
-        'Label5
+        'lblCantidad
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(230, 164)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(49, 13)
-        Me.Label5.TabIndex = 6
-        Me.Label5.Text = "Cantidad"
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Font = New System.Drawing.Font("Gill Sans MT", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCantidad.Location = New System.Drawing.Point(218, 164)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(101, 34)
+        Me.lblCantidad.TabIndex = 6
+        Me.lblCantidad.Text = "Cantidad"
         '
-        'Label6
+        'lblPrecioU
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(372, 164)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(72, 13)
-        Me.Label6.TabIndex = 7
-        Me.Label6.Text = "Precio unidad"
+        Me.lblPrecioU.AutoSize = True
+        Me.lblPrecioU.Font = New System.Drawing.Font("Gill Sans MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrecioU.Location = New System.Drawing.Point(335, 164)
+        Me.lblPrecioU.Name = "lblPrecioU"
+        Me.lblPrecioU.Size = New System.Drawing.Size(132, 30)
+        Me.lblPrecioU.TabIndex = 7
+        Me.lblPrecioU.Text = "Precio unidad"
         '
-        'Label7
+        'lblSubtotal
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(528, 164)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(46, 13)
-        Me.Label7.TabIndex = 8
-        Me.Label7.Text = "Subtotal"
+        Me.lblSubtotal.AutoSize = True
+        Me.lblSubtotal.Font = New System.Drawing.Font("Gill Sans MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubtotal.Location = New System.Drawing.Point(511, 164)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(85, 30)
+        Me.lblSubtotal.TabIndex = 8
+        Me.lblSubtotal.Text = "Subtotal"
         '
-        'Label8
+        'lblLugar
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(686, 164)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(87, 13)
-        Me.Label8.TabIndex = 9
-        Me.Label8.Text = "Lugar de compra"
+        Me.lblLugar.AutoSize = True
+        Me.lblLugar.Font = New System.Drawing.Font("Gill Sans MT", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLugar.Location = New System.Drawing.Point(634, 164)
+        Me.lblLugar.Name = "lblLugar"
+        Me.lblLugar.Size = New System.Drawing.Size(158, 30)
+        Me.lblLugar.TabIndex = 9
+        Me.lblLugar.Text = "Lugar de compra"
         '
-        'ListBox1
+        'lstNombre
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(54, 200)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(120, 147)
-        Me.ListBox1.TabIndex = 10
+        Me.lstNombre.FormattingEnabled = True
+        Me.lstNombre.Location = New System.Drawing.Point(33, 200)
+        Me.lstNombre.Name = "lstNombre"
+        Me.lstNombre.Size = New System.Drawing.Size(166, 147)
+        Me.lstNombre.TabIndex = 10
         '
-        'ListBox2
+        'lstCantidad
         '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(203, 200)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(120, 147)
-        Me.ListBox2.TabIndex = 11
+        Me.lstCantidad.FormattingEnabled = True
+        Me.lstCantidad.Location = New System.Drawing.Point(224, 200)
+        Me.lstCantidad.Name = "lstCantidad"
+        Me.lstCantidad.Size = New System.Drawing.Size(103, 147)
+        Me.lstCantidad.TabIndex = 11
         '
-        'ListBox3
+        'lstPrecio
         '
-        Me.ListBox3.FormattingEnabled = True
-        Me.ListBox3.Location = New System.Drawing.Point(347, 200)
-        Me.ListBox3.Name = "ListBox3"
-        Me.ListBox3.Size = New System.Drawing.Size(120, 147)
-        Me.ListBox3.TabIndex = 12
+        Me.lstPrecio.FormattingEnabled = True
+        Me.lstPrecio.Location = New System.Drawing.Point(340, 200)
+        Me.lstPrecio.Name = "lstPrecio"
+        Me.lstPrecio.Size = New System.Drawing.Size(127, 147)
+        Me.lstPrecio.TabIndex = 12
         '
-        'ListBox4
+        'lstSubtotal
         '
-        Me.ListBox4.FormattingEnabled = True
-        Me.ListBox4.Location = New System.Drawing.Point(494, 200)
-        Me.ListBox4.Name = "ListBox4"
-        Me.ListBox4.Size = New System.Drawing.Size(120, 147)
-        Me.ListBox4.TabIndex = 13
+        Me.lstSubtotal.FormattingEnabled = True
+        Me.lstSubtotal.Location = New System.Drawing.Point(494, 200)
+        Me.lstSubtotal.Name = "lstSubtotal"
+        Me.lstSubtotal.Size = New System.Drawing.Size(120, 147)
+        Me.lstSubtotal.TabIndex = 13
         '
-        'ComboBox1
+        'cmbLugar
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(651, 200)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox1.TabIndex = 14
+        Me.cmbLugar.FormattingEnabled = True
+        Me.cmbLugar.Location = New System.Drawing.Point(652, 200)
+        Me.cmbLugar.Name = "cmbLugar"
+        Me.cmbLugar.Size = New System.Drawing.Size(121, 21)
+        Me.cmbLugar.TabIndex = 14
         '
-        'Label9
+        'lblTotal
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(436, 369)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(31, 13)
-        Me.Label9.TabIndex = 15
-        Me.Label9.Text = "Total"
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(388, 366)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 39)
+        Me.lblTotal.TabIndex = 15
+        Me.lblTotal.Text = "Total"
         '
-        'TextBox3
+        'txtTotal
         '
-        Me.TextBox3.Location = New System.Drawing.Point(494, 366)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(120, 20)
-        Me.TextBox3.TabIndex = 16
+        Me.txtTotal.Location = New System.Drawing.Point(494, 366)
+        Me.txtTotal.Multiline = True
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(120, 39)
+        Me.txtTotal.TabIndex = 16
+        '
+        'btnFinalizar
+        '
+        Me.btnFinalizar.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btnFinalizar.Font = New System.Drawing.Font("Gill Sans MT", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFinalizar.ForeColor = System.Drawing.Color.Black
+        Me.btnFinalizar.Location = New System.Drawing.Point(652, 416)
+        Me.btnFinalizar.Name = "btnFinalizar"
+        Me.btnFinalizar.Size = New System.Drawing.Size(137, 40)
+        Me.btnFinalizar.TabIndex = 17
+        Me.btnFinalizar.Text = "Finalizar"
+        Me.btnFinalizar.UseVisualStyleBackColor = False
         '
         'Presupuestos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Salmon
-        Me.ClientSize = New System.Drawing.Size(873, 450)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.ListBox4)
-        Me.Controls.Add(Me.ListBox3)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.ClientSize = New System.Drawing.Size(802, 468)
+        Me.Controls.Add(Me.btnFinalizar)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.cmbLugar)
+        Me.Controls.Add(Me.lstSubtotal)
+        Me.Controls.Add(Me.lstPrecio)
+        Me.Controls.Add(Me.lstCantidad)
+        Me.Controls.Add(Me.lstNombre)
+        Me.Controls.Add(Me.lblLugar)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.lblPrecioU)
+        Me.Controls.Add(Me.lblCantidad)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.txtNoPedido)
+        Me.Controls.Add(Me.txtPresupuesto)
+        Me.Controls.Add(Me.lblNoPedido)
+        Me.Controls.Add(Me.lblPresupuesto)
+        Me.Controls.Add(Me.lblProductosLista)
         Me.Name = "Presupuestos"
         Me.Text = "Presupuestos"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
-    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
-    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
-    Friend WithEvents ListBox4 As System.Windows.Forms.ListBox
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
-    Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents lblProductosLista As System.Windows.Forms.Label
+    Friend WithEvents lblPresupuesto As System.Windows.Forms.Label
+    Friend WithEvents lblNoPedido As System.Windows.Forms.Label
+    Friend WithEvents txtPresupuesto As System.Windows.Forms.TextBox
+    Friend WithEvents txtNoPedido As System.Windows.Forms.TextBox
+    Friend WithEvents lblNombre As System.Windows.Forms.Label
+    Friend WithEvents lblCantidad As System.Windows.Forms.Label
+    Friend WithEvents lblPrecioU As System.Windows.Forms.Label
+    Friend WithEvents lblSubtotal As System.Windows.Forms.Label
+    Friend WithEvents lblLugar As System.Windows.Forms.Label
+    Friend WithEvents lstNombre As System.Windows.Forms.ListBox
+    Friend WithEvents lstCantidad As System.Windows.Forms.ListBox
+    Friend WithEvents lstPrecio As System.Windows.Forms.ListBox
+    Friend WithEvents lstSubtotal As System.Windows.Forms.ListBox
+    Friend WithEvents cmbLugar As System.Windows.Forms.ComboBox
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents btnFinalizar As System.Windows.Forms.Button
 End Class
--- Presupuestos.vb
+++ Presupuestos.vb
@@ -1,6 +1,14 @@
 ﻿Public Class Presupuestos
 
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblProductosLista.Click
+
+    End Sub
+
+    Private Sub ListBox3_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles lstPrecio.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbLugar.SelectedIndexChanged
 
     End Sub
 End Class
