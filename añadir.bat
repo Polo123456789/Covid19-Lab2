e49956533be39ee14a1a70a9618870c933852b99
Marco504 <marcoaguilon571@gmail.com>
2020-06-01 11:11:03 -0600

    Se creo el pedido de prueba para trabajar

--- añadir.bat
+++ añadir.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "alejandra paredes y ana gonzalez hicimos una base de datos de pedidos de access alli agragamos la tabla con los campos codigo de productos, productos, precio, cantidad y pusimos 5 ejemplos, luego lo unimos a visual basic e hicimos un fromulario con un diseño "
+git commit -m "Se creo el pedido de prueba para trabajar"
 git push
 PAUSE
