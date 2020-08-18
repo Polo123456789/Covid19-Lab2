6295e40e9436e8522a8d7182578276aad106b51e
Polo123456789 <57022222+Polo123456789@users.noreply.github.com>
2020-08-18 14:38:53 -0600

    Update Voluntarios.vb

--- Voluntarios.vb
+++ Voluntarios.vb
@@ -1,4 +1,21 @@
-﻿Public Class Voluntarios
+'  Guatemala en Casa
+'  Copyright (C) 2020 Pablo Sanchez, Marco Aguilon, Masiel Dieguez,
+'  Sebastian Letona, Any Gonzales, Alejandra Paredes.
+'
+'  This program is free software: you can redistribute it and/or modify
+'  it under the terms of the GNU General Public License as published by
+'  the Free Software Foundation, either version 3 of the License, or
+'  (at your option) any later version.
+'
+'  This program is distributed in the hope that it will be useful,
+'  but WITHOUT ANY WARRANTY; without even the implied warranty of
+'  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'  GNU General Public License for more details.
+'
+'  You should have received a copy of the GNU General Public License
+'  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Public Class Voluntarios
     Public NumPedido As Integer
     Private Sub Voluntarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'Base_Datos11DataSet.Pedido' table. You can move, or remove it, as needed.
